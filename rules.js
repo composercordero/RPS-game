c3485b075f5e09a84bf4e03d97a65b224eca8479
composercordero <composer.cordero@gmail.com>
2023-05-02 07:59:58 -0500

    Create project files, comment in Pseudocode in Js
    I created all the necessary files (HTML, CSS, and JS),
    adding to the HTML the necessary links.
    
    I commented in simple English the steps needed to play the game
    as well as brainstorm possible ideas for the future of the code.

---
+++ rules.js
@@ -0,0 +1,22 @@
+//Rock, Paper, Scissor - Rules
+
+//User against computer
+
+//User has three choices
+//User selects an option
+//Computer randomly selects an option
+//Evaluate result
+//Return result
+//Add points
+
+
+
+
+//Optional features for future iterations
+
+//Learn mode in which user can see what the computer will choose
+
+//Add Fire, Water Sponge, and Air
+
+/*Add the whole extended list of options for the game 
+(found at https://boardgames.stackexchange.com/questions/11280/adding-additional-weapons-to-rock-paper-scissors)*/
